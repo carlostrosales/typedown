b6e7ebb97651c802dc5853e53d060589a3eceb09
carlostrosales <rosales.carlost@Gmail.com>
2025-08-07 20:30:47 -0500

    feat: initialize state management for blocks in App component and pass blocks to BlockEditor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import { BlockEditor } from './BlockEditor';
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
+type Block = {
+  id: string;
+  content: string;
+  type: 'paragraph' | 'prompt' | 'response';
+};
+
+let arrayOfObjectBlocks: Block[] = [
+  {
+    id: '1',
+    content: 'Example 1',
+    type: 'paragraph',
+  },
+  {
+    id: '2',
+    content: 'Example 2',
+    type: 'paragraph',
+  },
+];
 
 function App() {
+  const [blocksArray, setBlocksArray] = useState<Block[]>(arrayOfObjectBlocks);
 
   return (
-    <BlockEditor />
-  )
+    <>
+      <BlockEditor blocksArray={blocksArray} onBlocksChange={setBlocksArray} />
+    </>
+  );
 }
 
 export default App;
